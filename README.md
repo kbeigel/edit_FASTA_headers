aeea28cade506a5b6e64c08a7e8e934f38c61127
Katherine Beigel <katherinebeigel@gmail.com>
2022-09-25 14:56:02 -0500

    Refined lines describing replacements and edits of the headers; updated comments throughout; formatting; removed pseudocode at the beginning

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
 # edit_FASTA_headers
 
 ## Description
-Searches for a string in FASTA header lines (lines that begin withg '>') and replaces instances of that string in the FASTA header lines. Outputs a new FASTA file with new FASTA headers.
+Searches for a string in FASTA header lines (lines that begin withg '>') and replaces instances substrings with new substrings / replaces string within a regex format / finds character and removes following characters in the FASTA header lines. Outputs a new FASTA file with new FASTA headers.
 
 ## Dependencies
   re
+
+
+
+
+## Pseudocode outline
+
+    # for every line with a '>', we want to get the ID and store that as the key.
+    # for every line after that line,
+    # if it DOES NOT have '>', then we want to append that line to the value.
+    build a dictionary
+    while the file still has lines:
+        look at the next line
+        does this line have a carrot?
+          get the ID out of the line
+          store in the dictionary as the key
+    while the file still has lines:
+      look at the next line
+      does this line not have a carrot?
+        tack on line to the value associated with the key
+    else stop
--- edit_FASTA_header.py
+++ edit_FASTA_header.py
@@ -1,86 +1,78 @@
+#!/bin/python
+
+# Import libraries
 import re
 
+################## MAIN ###################
+
 if __name__ == '__main__':
-    ## the infile name goes here, as a string in open()
+
+
+    # INPUT FILE
+    # the infile name goes here, as a string in open()
     infile = open('Ta_Ef1a_complete_genbank.fasta', 'r')
 
-    # for every line with a '>', we want to get the ID and store that as the key.
-    # for every line after that line,
-    # if it DOES NOT have '>', then we want to append that line to the value.
-    # if it does have '>', we want to stop.
 
+    # COLLECT HEADERS
+    # make a dictionary (ID will be keys and sequences will be values)
     fasta_dict = {}
 
-    # while the file still has lines:
-    # look at the next line
-    # does this line have a carrot?
-    # get the ID out of the line
-    # store in the dictionary as the key
-    # while the file still has lines
-    # look at the next line
-    # does this line not have a carrot?
-    # tack on line to the value associated with the key
-    # else stop
-
-    # currentLine = next(infile, "STOP").strip()
-    # while currentLine != "STOP":
-    #     if currentLine.startswith('>'):
-    #         fastaHeader = currentLine[1:]
-    #         fasta_dict[fastaHeader] = ''
-    #         currentLine = next(infile, "STOP").strip()
-    #         while currentLine != "STOP":
-    #             if not currentLine.startswith('>'):
-    #                 fasta_dict[fastaHeader] = fasta_dict[fastaHeader] + currentLine
-    #                 #print(fasta_dict)
-    #                 currentLine = next(infile, "STOP").strip()
-    #                 #print(currentLine)
-    #             else:
-    #                 break
-
-    previous_key = ''
+    # Collect header lines in the FASTA file
+    original_key = ''
     for current_line in infile.readlines():
+        # if the line starts with '>'
         if current_line.startswith('>'):
-            previous_key = current_line.strip()
-            fasta_dict[previous_key] = ''
+            # key is the line, with leading and trailing chars removed
+            original_key = current_line.strip()
+            # store key in dictionary
+            fasta_dict[original_key] = ''
+        # if the line does not start with '>'
         else:
-            fasta_dict[previous_key] += current_line.strip()
+            # remove trailing and leading whitespace, store as value for preceding line
+            fasta_dict[original_key] += current_line.strip()
 
+
+    # MAKE CHANGES / REPLACEMENTS TO HEADERS; OUTPUT EDITED HEADERS
+    # initialize a dictionary to store the output (edited) headers
     out_fasta_dict = {}
-    index = 1
+
+    # start index
+    index = 1 # use this if you want to number headers sequentially in file
+
+    # for each key-value pair in dictionary, do replacements,
+    # loop has some options for replacements (can use all or some)
     for key, value in fasta_dict.items():
-        # alternate method
-        # key = fasta_tuple[0]
-        # value = fasta_tuple[1]
+        
+        # 1. use specify strings to replace and replacements
         key = key.replace('-', '')
-        key = key.replace('>', '')
-        # key = key.replace('|revcompl', '')
+        key = key.replace('text_to_remove', '')
+
+        # 2. use to specify regex pattern to find
         match = re.search(r'[A-Za-z]+(?P<WiW>[0-9]{7})', key)
+        # if regex pattern is found
         if match is not None:
-            print(match)
-        key = key.replace(key, '[organism=Trachymyrmex arizonensis]' + ' wingless (wnt-1) gene, partial cds')
-        key = '>Seq' + str(index) + ' ' + key
-        index = index + 1
+            # update key with replacement string
+            key = key.replace(key, 'additional_text_to_add')
+            # update key to have 
+            key = 'Seq' + str(index) + '_' + key # elements can be reordered 
+            index += 1 # increment index (use if you want to add numbers)
 
-        # my first two lines by myself
-        # char_position = key.find('T')
-        # key = key[0:char_position]
+        # 3. remove everything after a certain character
+        char_position = key.find('C') # find first instance of char in line
+        # update key to be string from beginnning to searched char
+        key = key[0:char_position]
+
+        # output the updated keys to output dict 
+        # (using value (sequence) from previous dict)
         out_fasta_dict[key] = value
-        # new_key = key.split('_')[0]
 
-    # for key, value in out_fasta_dict.items():
-    #    if 'ITS' in key:
-    #        continue
-    #    else:
-    #        key = key + 'ITS'
 
+    # WRITE TO A NEW FILE
+    # specify a new file name
     new_file_name = 'Ta_Ef1a_complete_genbank_upload.fasta'
-    outfile = open(new_file_name, 'w')
-    # for key, value in out_fasta_dict:
-    #    outfile.write(key)
 
-    # same as above, less 'pythonic'
-    for key, value in out_fasta_dict.items():
+
+    # write a new file using the new file name
+    outfile = open(new_file_name, 'w')
+     for key, value in out_fasta_dict:
         outfile.write(key)
-        outfile.write('\n')
-        outfile.write(value)
-        outfile.write('\n')
